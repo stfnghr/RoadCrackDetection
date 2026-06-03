b8ec17983a657ff1770b3127a6243ea1f5ec4714
Stefanie Agahari <stefanie.agahari@gmail.com>
2026-06-03 12:43:29 +0700

    Initial Commit

---
+++ RoadCrackDetection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoadCrackDetection
+//
+//  Created by Stefanie Agahari on 03/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoadCrackDetection/RoadCrackDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadCrackDetectionApp.swift
+//  RoadCrackDetection
+//
+//  Created by Stefanie Agahari on 03/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RoadCrackDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
